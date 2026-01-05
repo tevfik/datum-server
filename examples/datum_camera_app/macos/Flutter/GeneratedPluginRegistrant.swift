d5010da2a6ee0c6b2a372b398afe6c7dc9af4802
tevfik <tevfik.kadioglu@gmail.com>
2026-01-05 23:40:04 +0300

    feat: cleanup unused flutter packages and update dependencies

--- examples/datum_camera_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/datum_camera_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,9 +10,7 @@ import flutter_quick_video_encoder
 import gal
 import package_info_plus
 import path_provider_foundation
-import share_plus
 import shared_preferences_foundation
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
@@ -20,7 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
--- examples/datum_camera_app/windows/flutter/generated_plugin_registrant.cc
+++ examples/datum_camera_app/windows/flutter/generated_plugin_registrant.cc
@@ -8,19 +8,10 @@
 
 #include <app_links/app_links_plugin_c_api.h>
 #include <gal/gal_plugin_c_api.h>
-#include <permission_handler_windows/permission_handler_windows_plugin.h>
-#include <share_plus/share_plus_windows_plugin_c_api.h>
-#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AppLinksPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AppLinksPluginCApi"));
   GalPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GalPluginCApi"));
-  PermissionHandlerWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
-  SharePlusWindowsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
-  UrlLauncherWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
